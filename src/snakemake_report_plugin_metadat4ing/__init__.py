986f7a3ac4b5dc9576bd1cbc7aeda33d344d2b12
M-Jafarkhani <mahdi.jafarkhani@gmail.com>
2025-05-09 17:19:50 +0200

    Add input support

--- src/snakemake_report_plugin_metadat4ing/__init__.py
+++ src/snakemake_report_plugin_metadat4ing/__init__.py
@@ -5,6 +5,7 @@ from snakemake_interface_report_plugins.settings import ReportSettingsBase
 from rdflib import Graph
 import requests
 import json
+import os
 
 @dataclass
 class ReportSettings(ReportSettingsBase):
@@ -17,7 +18,7 @@ class Reporter(ReporterBase):
     def render(self):
         self.get_context()
         jobs_dict = {}
-        output_files_dict = {}
+        files_dict = {}
         main_processing_steps_dict = {}
         file_counter = 0
         
@@ -26,9 +27,12 @@ class Reporter(ReporterBase):
             "@graph": []
         }
         jsonld['@context']['local'] = "https://local-domain.org/"
-              
+           
         sorted_jobs = sorted(self.jobs, key=lambda job: job.starttime)
         main_processing_steps = {processing_steps.rule for processing_steps in self.jobs}
+        
+        for job in self.dag.jobs:
+            print(f"Job: {job.jobid} -- {job.input}")
         
         for i, processing_step in enumerate(main_processing_steps):
             item = {
@@ -47,20 +51,40 @@ class Reporter(ReporterBase):
                 "part of": {"@id": main_processing_steps_dict[job.rule]["@id"]},
                 "start time": f"{datetime.fromtimestamp(job.starttime)}",
                 "end time": f"{datetime.fromtimestamp(job.endtime)}",
+                "has input": [],
                 "has output": []
             }
+            
+            for _job in self.dag.jobs:
+                if _job.jobid == job.job.jobid:
+                    for file in _job.input:
+                        if file not in files_dict:
+                            files_dict[file] = {
+                                "@id": f"local:file_{file_counter}",
+                                "@type": "cr:FileObject",
+                                "label": file
+                            }
+                            file_counter += 1
+                            params = self.extract_params(job.rule, file)
+                            if params != {}:
+                                print(f"Parameters for {file}: {params}")
+                    item["has input"].append({"@id": files_dict[file]["@id"]})
+            
             for file in job.output:
-                if file not in output_files_dict:
-                    output_files_dict[file] = {
-                        "@id": f"local:output_file_{file_counter}",
+                if file not in files_dict:
+                    files_dict[file] = {
+                        "@id": f"local:file_{file_counter}",
                         "@type": "cr:FileObject",
                         "label": file
                     }
                     file_counter += 1
-                item["has output"].append({"@id": output_files_dict[file]["@id"]})
+                    params = self.extract_params(job.rule, file)
+                    if params != {}:
+                        print(f"Parameters for {file}: {params}")
+                item["has output"].append({"@id": files_dict[file]["@id"]})
             jobs_dict[job_label] = item
         
-        for node in (main_processing_steps_dict, jobs_dict, output_files_dict):
+        for node in (main_processing_steps_dict, jobs_dict, files_dict):
             jsonld["@graph"].extend(node.values())
         
         with open("report.jsonld", "w", encoding='utf8') as file:
@@ -77,6 +101,27 @@ class Reporter(ReporterBase):
             print(f"Failed to fetch context data. Status code: {response.status_code}")
             self.context_data = {}
     
+    def extract_params(self, rule_name:str, file_path:str):
+        result = {}
+        json_data = {}
+        file_name = os.path.basename(file_path)
+        #print(f"Extracting parameters from {file_name} for rule {rule_name}")
+        if file_name.startswith("parameters_") and rule_name == "generate_input_files":
+            with open(file_path) as f:
+                json_data = json.load(f)
+            for key, item in json_data.items():
+                if isinstance(item, dict):
+                    result[key] = {
+                        'Value': item['value'],
+                        'Unit': item['unit']
+                    }
+                else:
+                    result[key] = {
+                        'Value': item,
+                        'Unit': None
+                    }
+        return result
+            
     def add_dependencies(self):
         # for target_job, dependent_jobs in self.dag.dependencies.items():
         #     for dependent_job in dependent_jobs:
