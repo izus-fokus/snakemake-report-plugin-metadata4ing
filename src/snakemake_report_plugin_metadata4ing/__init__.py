9a0b6b3259cf1980394b4039dbeeb873142211cd
M-Jafarkhani <mahdi.jafarkhani@gmail.com>
2025-08-29 17:57:56 +0200

    Refactor, removing CI's

--- src/snakemake_report_plugin_metadata4ing/__init__.py
+++ src/snakemake_report_plugin_metadata4ing/__init__.py
@@ -91,7 +91,7 @@ class Reporter(ReporterBase):
             
         with open("provenance.jsonld", "w", encoding="utf8") as f:
             json.dump(jsonld, f, indent=4, ensure_ascii=False)
-        print(self.child_nodes)
+        
         self._create_ttl_from_jsonld(jsonld)
         self._add_ro_crate_file_nodes(file_nodes)
         self._create_ro_crate_file()
