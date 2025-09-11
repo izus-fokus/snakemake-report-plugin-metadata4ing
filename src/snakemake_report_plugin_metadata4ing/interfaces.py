ac360f0cf73f2acfd64800e3e2b19f097330bb12
M-Jafarkhani <mahdi.jafarkhani@gmail.com>
2025-09-11 18:29:58 +0200

    Fix abstraction

--- src/snakemake_report_plugin_metadata4ing/interfaces.py
+++ src/snakemake_report_plugin_metadata4ing/interfaces.py
@@ -4,7 +4,3 @@ class ParameterExtractorInterface(ABC):
     @abstractmethod
     def extract_params(self, rule_name: str, file_path: str) -> dict:
         ...
-    
-    @abstractmethod
-    def extract_tools(self, rule_name: str, env_file_content: str) -> dict:
-        ...    
